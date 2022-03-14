0825d6878dd466b2c0763daf1edbf6b7562981b5
newton <soupjn@gmail.com>
2022-03-14 16:22:49 -0700

    Basic interpreter skeleton

--- app/src/main/java/jlox/Lox.java
+++ app/src/main/java/jlox/Lox.java
@@ -12,7 +12,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+  private static final Interpreter interpreter = new Interpreter();
   static boolean hadError = false;
+  static boolean hadRuntimeError = false;
   
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
@@ -31,6 +33,7 @@ public class Lox {
 
     // Indicate an error in the exit code;
     if (hadError) System.exit(65);
+    if (hadRuntimeError) System.exit(70);
   }
 
   private static void runPrompt() throws IOException {
@@ -50,10 +53,13 @@ public class Lox {
   private static void run(String source) {
     Scanner scanner = new Scanner(source);
     List<Token> tokens = scanner.scanTokens();
+    Parser parser = new Parser(tokens);
+    Expr expression = parser.parse();
 
-    for (Token token : tokens) {
-      System.out.println(token);
-    }
+    // Stop if there was a syntax error
+    if (hadError) return;
+
+    interpreter.interpret(expression);
   }
 
   static void error(int line, String message) {
@@ -64,5 +70,18 @@ public class Lox {
     System.err.println(
                        "[line " + line + "] Error" + where + ": " + message);
     hadError = true;
+  }
+
+  static void error(Token token, String message) {
+    if (token.type == TokenType.EOF) {
+      report(token.line, " at end", message);
+    } else {
+      report(token.line, "at '" + token.lexeme + "'", message);
+    }
+  }
+
+  static void runtimeError(RuntimeError error) {
+    System.err.println(error.getMessage() +
+                       "\n[line " + error.token.line + "]");
   }
 }
