acaaa2238bf2ed3a206fcc2eff336ca583f57bb1
newton <newton@newtons-mbp.lan>
2022-05-08 19:03:25 -0700

    Add class constructor

--- app/src/main/java/jlox/LoxClass.java
+++ app/src/main/java/jlox/LoxClass.java
@@ -3,7 +3,7 @@ package jlox;
 import java.util.List;
 import java.util.Map;
 
-class LoxClass {
+class LoxClass implements LoxCallable {
   final String name;
   private final Map<String, LoxFunction> methods;
 
@@ -23,5 +23,24 @@ class LoxClass {
   @Override
   public String toString() {
     return name;
+  }
+
+  @Override
+  public Object call(Interpreter interpreter,
+                     List<Object> arguments) {
+    LoxInstance instance = new LoxInstance(this);
+    LoxFunction initializer = findMethod("init");
+    if (initializer != null) {
+      initializer.bind(instance).call(interpreter, arguments);
+    }
+    
+    return instance;
+  }
+
+  @Override
+  public int arity() {
+    LoxFunction initializer = findMethod("init");
+    if (initializer == null) return 0;
+    return initializer.arity();
   }
 }
--- app/src/main/java/jlox/Resolver.java
+++ app/src/main/java/jlox/Resolver.java
@@ -16,6 +16,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   private enum FunctionType {
     NONE,
+    INITIALIZER,
     METHOD
   }
 
@@ -47,6 +48,9 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     for (Stmt.Function method : stmt.methods) {
       FunctionType declaration = FunctionType.METHOD;
+      if (method.name.lexeme.equals("init")) {
+        declaration = FunctionType.INITIALIZER;
+      }
       resolveFunction(method, declaration);
     }
 
@@ -194,6 +198,11 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     }
 
     if (stmt.value != null) {
+      if (currentFunction == FunctionType.INITIALIZER) {
+        Lox.error(stmt.keyword,
+                  "Can't return a vaule from an initializer.");
+      }
+      
       resolve(stmt.value);
     }
     return null;
